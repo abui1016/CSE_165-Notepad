f8501aaab7a6c36d004d5c9f6eac7ab26739fabe
laminesunga <78405929+laminesunga@users.noreply.github.com>
2021-04-28 19:24:37 -0700

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -60,6 +60,26 @@ void MainWindow::on_actionUndo_triggered()
 // Get Word Count
 void MainWindow::on_actionWord_Count_triggered()
 {
+    int word_count=0;  // store the number of word
+    int char_count=0;  // store the number of character
+  
+
+    QString text=ui->textEdit->toPlainText();  // get the text as a string fron the text edit
+    text=text.simplified();      // method to remove the white space at the start and end, and cut any sequence of white space into one.
+
+    while(text.endsWith(' ')||text.endsWith('\n')) text.chop(1); // check and remove the white space and the return line (\n) at the satr, end of the Qstring
+    
+
+    word_count = 1+text.count(QLatin1Char(' ')) + text.count(QLatin1Char('\n')); // Here we count the number of white space which is the number of word
+
+   char_count= ui->textEdit->toPlainText().length();// not useful
+
+   if (char_count==0){
+       word_count=0;
+   }
+    
+    QString status = QString("Found %1 words:").arg(word_count);
+    QMessageBox::information(this, tr("Info"), status);
 
 }
 
